f8fc5bcfcd348e1ca3afbc84aeafcabd1c87c514
Ricardo D'Alessandro <rgda.geo@yahoo.com>
2016-07-08 10:57:50 -0500

    First try at creating the mongodb model

---
+++ server/apiModels/articles.js
@@ -0,0 +1,43 @@
+var db = require('../db');
+
+var util = require('../lib/utility');
+
+var Article = module.exports;
+
+Article.all = function () {
+  return db.collection('articles').find({});
+};
+
+Article.allIds = function () {
+  return db.collection('articles').find({}, {_id: 1});
+};
+
+Article.findByDate = function (startDate, endDate) {
+  return db.collection('articles').find({
+    pubDate: {
+      $gte: ISODate(startDate),
+      $lt: ISODate(endDate)
+    }
+  });
+};
+
+Article.create = function (incomingArticles) {
+  // Copy to avoid mutation
+  var attrs = Object.assign({}, incomingArticles);
+  var existingArticles = [];
+
+  Article.allIds().then(function(rows){
+    existingArticles = rows.map(function(row){
+      return row._id;
+    });
+    insert();
+  });
+
+  function insert() {
+    for (article of incomingArticles) {
+      if (existingArticles.indexOf(article._id) < 0) {
+        db.collection('articles').insert(article);
+      }
+    }
+  }
+};
