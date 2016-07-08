7f5f72f5190fd39bdb1edfd27521b8a5c4072007
Tony Kung <lps.tony@gmail.com>
2016-07-08 11:43:28 -0500

    Revert "Created basic features for Splash"
    This reverts commit 042bafe83f9d51a087d41db2a31b8318fcf35d42.

--- client/components/Splash.js
+++ client/components/Splash.js
@@ -1,32 +1,15 @@
 // this is the 'landing page' component
 import React from 'react';
-import ReactDOM from 'react-dom'; 
 
-export default class Splash extends React.Component{
+export default class ArticleList extends React.Component{
   constructor(props) {
     super(props);
 
-   
+    // 
   }
 
-  render(){
-
-  	const now = new Date(); 
-  	return (
-  		<div>
-  			<h1>Good News!!!</h1>
-  			<div>
-  				<h3>Today's Stories</h3>
-  				Current time: {now.toTimeString()}
-  				<button className='moodSetter'>Set Your Mood!</button>
-  			</div>
-  		</div>); 
-  }
-
-
- ReactDOM.render() {
+  render() {
     // welcome people to our site, and have button to see articles and set mood = cheerful;
     // login form
-    <Splash />, document.getElementById('app');
   }
 }
