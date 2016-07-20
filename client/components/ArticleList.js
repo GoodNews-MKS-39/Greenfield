c361ad89f09ef7e66f41871a125058f1feba5250
Jordan Campbell <jcampbell1441@gmail.com>
2016-07-20 15:36:11 -0500

    Sorting by mood state

--- client/components/ArticleList.js
+++ client/components/ArticleList.js
@@ -10,7 +10,8 @@ export default class ArticleList extends React.Component {
     super(props);
 
     this.state = {
-      articles: []
+      articles: [],
+      mood: 'good'
     };
   }
   onlyUnique(value, index, self) { 
@@ -43,6 +44,40 @@ export default class ArticleList extends React.Component {
       audio.play();
     })
   }
+  renderArticles(articles) {
+    console.log("Articles front end: 48:", articles)
+    // sorts articles by emotion score by what the current mood is.
+    if(this.state.mood){
+      if(this.state.mood === 'good'){
+        articles.sort((a, b) => {
+          if(a.sentimentScore > b.sentimentScore) return -1;
+          else if(b.sentimentScore > a.sentimentScore) return 1;
+          else return 0;
+        })
+      } else if(this.state.mood === 'bad'){
+        articles.sort((a, b) => {
+          if(a.sentimentScore > b.sentimentScore) return 1;
+          else if(b.sentimentScore > a.sentimentScore) return -1;
+          else return 0;
+        })
+      }
+    }
+    // Returning article elements to be displayed
+    return articles.map((article) => {
+      return (
+        <div className="col-sm-6 col-md-4">
+          <div className='single_article'>
+            <img src={article.urlToImage} />
+            <h3> { article.title } - { article.publishedAt }</h3>
+            <div className="article_p">
+              <p> { article.description } <a href={article.url} target="_blank">(Read more)</a></p>
+            </div>
+            <button onClick={this.textToSpeech.bind(null, article.description)}> Hear </button>
+          </div>
+        </div>
+      )
+    })
+  }
 
   render() {
     // show all articles for the given time period (eg. today) filtered for the mood variable in the app component
@@ -52,23 +87,7 @@ export default class ArticleList extends React.Component {
           <h1>Good News</h1>
           <UserControls getArticles={this.getArticles.bind(this)} articles={this.state.articles}/>
         </div> 
-        {this.state.articles
-          .map((article) => {
-
-            return (
-              <div className="col-sm-6 col-md-4">
-                <div className='single_article'>
-                  <img src={article.urlToImage} />
-                  <h3> { article.title } - { article.publishedAt }</h3>
-                  <div className="article_p">
-                    <p> { article.description } <a href={article.url} target="_blank">(Read more)</a></p>
-                  </div>
-                  <button onClick={this.textToSpeech.bind(null, article.description)}> Hear </button>
-                </div>
-              </div>
-            )
-          })
-        }
+        {this.renderArticles(this.state.articles)}
       </div>
     )
   }
