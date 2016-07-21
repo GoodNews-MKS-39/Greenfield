464d22084bf85aa7bf32b8c5182c86d39198073e
davidlewallen <lewallen.david@gmai.com>
2016-07-21 11:17:47 -0500

    added class names to the buttons

--- client/components/UserControls.js
+++ client/components/UserControls.js
@@ -5,8 +5,8 @@ export default class UserControls extends React.Component {
   render() {
     return (
       <div className='user-controls'>
-        <button className='goodNews' onClick={this.props.getArticles}>Good News</button>
-        <button className='badNews' onClick={this.props.getArticles}>Bad News</button>
+        <button className='pure-button pure-button-primary goodNews' onClick={this.props.getArticles}>Good News</button>
+        <button className='pure-button pure-button-primary badNews' onClick={this.props.getArticles}>Bad News</button>
       </div>
     )
   }
