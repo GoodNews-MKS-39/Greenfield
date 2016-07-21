1a58fdb9354cad9863d3a488827210883ad54c27
Caleb Anderson <caleb.anderson130@gmail.com>
2016-07-21 11:55:24 -0500

    fix conditional in comments.js

--- server/comments.js
+++ server/comments.js
@@ -5,25 +5,29 @@ var Comments = module.exports;
 //change these to db.collection('comments') for deployment or if youre getting erros
 
 Comments.findByTitle = function (title) {
-	if(db.collection('comments')) {
-		return db.collection('comments').find({title: title})
-	} else {
-		return db.comments.find({titile: title})
-	}
+	db.collection('comments').then(x => {
+    if(x) {
+  		return db.collection('comments').find({title: title})
+  	} else {
+  		return db.comments.find({titile: title})
+  	}
+  })
 }
 
 Comments.newComment = function (title, username, msg) {
-	if(db.collectio('comments')) {	
-		return db.collection('comments').insert({
-			title: title,
-			username: username,
-			msg: msg
-		})
-	} else {
-		return db.comments.insert({
-			title: title,
-			username: username,
-			msg: msg
-		})
-	}
+  db.collection('comments').then(x => {
+  	if(x) {	
+  		return db.collection('comments').insert({
+  			title: title,
+  			username: username,
+  			msg: msg
+  		})
+  	} else {
+  		return db.comments.insert({
+  			title: title,
+  			username: username,
+  			msg: msg
+  		})
+  	}
+  })
 }
