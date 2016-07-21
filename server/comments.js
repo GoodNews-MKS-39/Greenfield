db1f5fcecbce77ae655df84769e84acdcd65691e
Caleb Anderson <caleb.anderson130@gmail.com>
2016-07-21 11:58:54 -0500

    edit comments js and remove console.logs

--- server/comments.js
+++ server/comments.js
@@ -5,29 +5,13 @@ var Comments = module.exports;
 //change these to db.collection('comments') for deployment or if youre getting erros
 
 Comments.findByTitle = function (title) {
-	db.collection('comments').then(x => {
-    if(x) {
-  		return db.collection('comments').find({title: title})
-  	} else {
-  		return db.comments.find({titile: title})
-  	}
-  })
+	return db.collection('comments').find({title: title})
 }
 
 Comments.newComment = function (title, username, msg) {
-  db.collection('comments').then(x => {
-  	if(x) {	
-  		return db.collection('comments').insert({
-  			title: title,
-  			username: username,
-  			msg: msg
-  		})
-  	} else {
-  		return db.comments.insert({
-  			title: title,
-  			username: username,
-  			msg: msg
-  		})
-  	}
-  })
+	return db.collection('comments').insert({
+		title: title,
+		username: username,
+		msg: msg
+	})
 }
