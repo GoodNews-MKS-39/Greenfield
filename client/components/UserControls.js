d3fd3fbb193d5f2d915eadb62c3e9d0b7230b172
davidlewallen <lewallen.david@gmai.com>
2016-07-21 14:16:37 -0500

    Added purecss to each button

--- client/components/UserControls.js
+++ client/components/UserControls.js
@@ -10,8 +10,8 @@ export default class UserControls extends React.Component {
   render() {
     return (
       <div className='user-controls'>
-        <button className='goodNews' onClick={this.handleButtonClick.bind(this, "good")}>Good News</button>
-        <button className='badNews' onClick={this.handleButtonClick.bind(this, "bad")}>Bad News</button>
+        <button className='pure-button pure-button-primary goodNews' onClick={this.handleButtonClick.bind(this, "good")}>Good News</button>
+        <button className='pure-button pure-button-primary badNews' onClick={this.handleButtonClick.bind(this, "bad")}>Bad News</button>
       </div>
     )
   }
